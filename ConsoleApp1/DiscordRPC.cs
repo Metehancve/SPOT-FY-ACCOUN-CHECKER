b61efe957378fbd7a36d450c9f42c6b28f5c4313
Metehancve <109982134+Metehancve@users.noreply.github.com>
2022-07-25 20:50:52 +0300

    CT SPOTİFY CHECKER

---
+++ ConsoleApp1/DiscordRPC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordRPC;
+
+namespace ConsoleApp1
+{
+	class DiscordRPC
+	{
+		public static DiscordRpcClient client;
+
+		public static void Initialize()
+		{
+			
+            client = new DiscordRpcClient("1001074622030151680");
+            client.Initialize();
+            client.SetPresence(new RichPresence()
+			{
+				Details = "CRACKTURKEY Spotify Checker",
+				State = "Spotfiy Checking",
+				Assets = new Assets()
+				{
+					LargeImageKey = "indir_1_",
+					LargeImageText = ""
+				}
+			});
+			
+
+		}
+	}
+}
